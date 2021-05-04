ab6605cb1260a96277d2c311f53e544451a9c29c
Antonio Ortiz <tototony@me.com>
2021-05-04 00:24:19 -0500

    change to lambda

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-require('dotenv').config();
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
 
+exports.handler = async (event) => {
+    
+    console.log("EVENT: \n" + JSON.stringify(event, null, 2))
 
-const express = require('express');
-const app = express();    
-const cors = require('cors');
-
-const PORT = process.env.PORT || 8080
-const HOST = process.env.HOST || "0.0.0.0"
-
-app.use(express.json())
-app.use(cors())
-
-app.listen(PORT, (err) => {
-    if (err) {
-      throw err
-    }
-    console.log('Servidor iniciado en el puerto ' + PORT)
-  })
+    
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+      version: '2020-08-01',
+      authenticator: new IamAuthenticator({
+        apikey: 'process.env.API_KEY',
+      }),
+      serviceUrl: 'https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/2f859a53-7b7f-4d32-afb1-384d016fbe96',
+    });
+    
+      
+      const analyzeParams = {
+        'url': 'www.ibm.com',
+        'features': {
+          'categories': {
+            'limit': 3
+          }
+        }
+      };
+      
+      naturalLanguageUnderstanding.analyze(analyzeParams)
+        .then(analysisResults => {
+          console.log(JSON.stringify(analysisResults, null, 2));
+        })
+        .catch(err => {
+          console.log('error:', err);
+        });
+      
+};
 
--- index_.js
+++ index_.js
@@ -1,18 +1,57 @@
-'use strict'
+require('dotenv').config();
 
-const express = require('express')
-const app = express()
 
-const PORT = process.env.PORT || 8080
-const HOST = process.env.HOST || "0.0.0.0"
+const express = require('express');
+const app = express();    
+const cors = require('cors');
 
-app.use(express.static(__dirname + '/public'))
+const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+
 app.use(express.json())
+app.use(cors())
+app.set('view engine', 'pug')
 
 
-app.listen(PORT, (err) => {
+app.listen(8080, (err) => {
     if (err) {
       throw err
     }
-    console.log('Servidor iniciado en el puerto ' + PORT)
+    console.log('Servidor iniciado en el puerto 8080')
   })
+
+app.post('/analizar', (req, res) => {
+    console.log(req.body)
+
+    const toneAnalyzer = new ToneAnalyzerV3({
+        version: '2017-09-21',
+        authenticator: new IamAuthenticator({
+          apikey: process.env.API_KEY,
+        }),
+        serviceUrl: 'https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/271700cf-6143-4817-8a24-ceb8420a2625',
+      });
+
+    const text = req.body.text;    
+
+    const toneParams = {
+        toneInput: { 'text': text },
+        contentType: 'application/json',
+    };
+
+    toneAnalyzer.tone(toneParams)
+    .then(toneAnalysis => {
+        app.get('/resultado', (req,res) => {
+            res.render('resultado', { mensaje: (JSON.stringify(toneAnalysis, null, 2)) })
+        })
+        console.log(JSON.stringify(toneAnalysis, null, 2));
+    })
+    .catch(err => {
+        app.get('/resultado', (req,res) => {
+            res.render('resultado', { mensaje: ('error:', err) })
+        })
+        console.log('error:', err);
+    });
+
+})
+
